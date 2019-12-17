21d0a574d695129b1b6537bddbce646424638e04
Johan <johan225@hotmail.es>
2019-12-17 17:02:18 -0500

    Agrega session

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-
+const cookieSession = require('cookie-session');
 const User = require("./models/User")
 app.set("view engine", "pug");
 app.set("views", "views")
 app.use(express.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  secret: "una_cadena_secreta",
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("error",function(e){console.error(e);});
 
+const requireUser = async (req, res, next) => {
+  const userId = req.session.userId;
+  if (userId) {
+    const user = await User.findOne({ _id: userId });
+    res.locals.user = user;
+    next();
+  } else {
+    return res.redirect('/login');
+  }
+}
 
-app.get('/', async (req, res) => {
+app.get('/', requireUser, async (req, res) => {
   const users = await User.find();
   res.render("index", { users})
 });
@@ -45,6 +61,7 @@ app.post("/login", async (req, res, next) => {
   try {
     const user = await User.authenticate(req.body.email, req.body.password);
     if (user) {
+      req.session.userId = user._id;
       return res.redirect("/");
     }else {
       res.redirect("/login")
